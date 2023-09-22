c9d153d4d620f106c740999a7958b145abd100d9
RachelS <rae72sch@hotmail.com>
2023-09-22 17:48:47 +0100

    Initial bare bones App file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import styles from './App.module.css';
+
+// The 3 modules
+import SearchBar from './SearchBar/SearchBar';
+import SearchResults from './SearchResults/SearchResults';
+import Playlist from './Playlist/Playlist';
 
 function App() {
+
+  const[searchResults, setSearchResults] = useState([
+    {
+      "name": "The Longships",
+      "artist": "Enya",
+      "album": "Watermark",
+      "id": 1
+    },
+    {
+      "name": "So Far Away",
+      "artist": "Dire Straits",
+      "album": "Brothers In Arms",
+      "id": 2
+    },
+    {
+      "name": "Layla",
+      "artist": "Eric Clapton",
+      "album": "Unplugged",
+      "id": 3
+    },
+    {
+      "name": "Zombie",
+      "artist": "The Cranberries",
+      "album": "No Need To Argue",
+      "id": 4
+    }
+  ]);
+  const[playlistTracks, setPlaylistTracks] = useState([
+    {
+      "name": "The Longships",
+      "artist": "Enya",
+      "album": "Watermark",
+      "id": 1
+    },
+    {
+      "name": "So Far Away",
+      "artist": "Dire Straits",
+      "album": "Brothers In Arms",
+      "id": 2
+    },
+    {
+      "name": "Layla",
+      "artist": "Eric Clapton",
+      "album": "Unplugged",
+      "id": 3
+    },
+    {
+      "name": "Zombie",
+      "artist": "The Cranberries",
+      "album": "No Need To Argue",
+      "id": 4
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <SearchBar />
+        <div className={ styles.container }>
+          <SearchResults searchResults={searchResults} />
+          <Playlist playlistTracks={playlistTracks} />
+        </div>
+      </div>
   );
 }
 
 export default App;
+
+// Photo by <a href="https://unsplash.com/@reallynattu?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nattu Adnan</a> on <a href="https://unsplash.com/photos/Ai2TRdvI6gM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
