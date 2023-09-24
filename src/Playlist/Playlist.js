3bd93ecf76082002ab4729356306973252ffd42d
RachelS <rae72sch@hotmail.com>
2023-09-24 06:29:19 +0100

    Functionality to remove track listings added

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from './Playlist.module.css';
 import Tracklist from '../Tracklist/Tracklist';
 
-export default function Playlist() {
+export default function Playlist(props) {
+    const handleNameChange = useCallback(
+        (event) => {
+          props.onNameChange(event.target.value);
+        },
+        [props.onNameChange]
+    );
+    
     return (
         <div className={ styles.playlist }>
-            <h2>Playlist</h2>
-            <hr />
-            
-            <form>
-                <input name="submit" type="submit"  value="Save to Spotify" className={ styles.button } />
-            </form>
+            <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+            <br />
+            <Tracklist tracks={props.playlistTracks} isRemoval={true} onRemove={props.onRemove} />
+            <p>
+                <button className={ styles.button } onClick={props.onSave}>
+                    Save To Spotify
+                </button>
+            </p>
         </div>
-    )
+    );
 }
