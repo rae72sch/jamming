0a345e362413ec251c9b3ce1cda357d268ee90ec
RachelS <rae72sch@hotmail.com>
2023-09-24 07:08:20 +0100

    Added useCallback functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import styles from './App.module.css';
 
@@ -7,8 +7,7 @@ import SearchBar from './SearchBar/SearchBar';
 import SearchResults from './SearchResults/SearchResults';
 import Playlist from './Playlist/Playlist';
 
-function App() {
-
+export default function App() {
   const[searchResults, setSearchResults] = useState([
     {
       "name": "The Longships",
@@ -35,45 +34,37 @@ function App() {
       "id": 4
     }
   ]);
-  const[playlistTracks, setPlaylistTracks] = useState([
-    {
-      "name": "The Longships",
-      "artist": "Enya",
-      "album": "Watermark",
-      "id": 1
+  const[playlistTracks, setPlaylistTracks] = useState([]);
+
+  const addTrack = useCallback(
+    (track) => {
+      if (playlistTracks.some((savedTrack) => savedTrack.id === track.id))
+        return;
+
+      setPlaylistTracks((prevTracks) => [...prevTracks, track]);
     },
-    {
-      "name": "So Far Away",
-      "artist": "Dire Straits",
-      "album": "Brothers In Arms",
-      "id": 2
-    },
-    {
-      "name": "Layla",
-      "artist": "Eric Clapton",
-      "album": "Unplugged",
-      "id": 3
-    },
-    {
-      "name": "Zombie",
-      "artist": "The Cranberries",
-      "album": "No Need To Argue",
-      "id": 4
-    }
-  ]);
+    [playlistTracks]
+  );
+
+  const removeTrack = useCallback((track) => {
+    setPlaylistTracks((prevTracks) =>
+      prevTracks.filter((currentTrack) => currentTrack.id !== track.id)
+    );
+  }, []);
 
   return (
       <div>
         <SearchBar />
         <div className={ styles.container }>
-          <SearchResults searchResults={searchResults} />
-          <Playlist playlistTracks={playlistTracks} />
+          <SearchResults searchResults={searchResults}  onAdd={addTrack} />
+          <Playlist 
+            playlistTracks={playlistTracks}
+            onRemove={removeTrack}
+          />
         </div>
       </div>
   );
 }
-
-export default App;
 
 // Photo by <a href="https://unsplash.com/@reallynattu?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nattu Adnan</a> on <a href="https://unsplash.com/photos/Ai2TRdvI6gM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
